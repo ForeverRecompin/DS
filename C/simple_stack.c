60895588fd81f9dc980324371eec06a20a667260
Abhijit Jain <abhijitjain.aj@gmail.com>
2015-06-03 13:39:25 +0530

    Wrote simple_stack.c

--- C/simple_stack.c
+++ C/simple_stack.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX_ELEMENTS 10
-#define TRUE 1
-#define FALSE 0
 
 struct array_stack
 {
@@ -47,32 +45,25 @@ int main()
 	s->top = -1;
 	s->array = malloc(s->capacity * sizeof(int));
 	int option,data;
-	
-	while(TRUE)
+	while(1)
 	{
 		printf("1: isEmpty()\n2: isFull()\n3: push(...)\n4: pop()\n5: Exit\n");
-		if((scanf("%d",&option)) == 1)
-		{
-		    if (option==1)
+		scanf("%d", &option);
+		if(d)
+		if (option==1)
 			printf("\t%d\n",isEmpty());
-		    else if (option==2)
+		else if (option==2)
 			printf("\t%d\n",isFull());
-		    else if (option==3)
-		    {	
+		else if (option==3)
+		{	
 			printf("\t"); 
 			scanf("\t%d",&data);
 			push(data);
-		    }
-		    else if (option==4)
+		}
+		else if (option==4)
 			printf("\t%d\n",pop());
-		    else if (option==5)
+		else if (option==5)
 			break;
-		}
-		else
-		{
-		    printf("Bad input.Quitting.\n");
-		    break;
-		}
 	}
 
 }
