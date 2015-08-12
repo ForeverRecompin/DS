b1dc42cde330715a5302f65740a66bb179896afc
A Jain <foreverrecompin@gmail.com>
2015-08-12 22:28:32 +0530

    Corrected perfect power using binary search algorithm

--- python/perfect_power_check_binary_search_approach.py
+++ python/perfect_power_check_binary_search_approach.py
@@ -1,13 +1,19 @@
 import math
 
 def per_pow(n):
-    f, l = 2, n
-    while(f <= l):
-        m = f + int((l-f)/2)
-        if n == m ** int(math.log(n,m)):
-            return True
-        elif n < m ** int(math.log(n,m)):
-            l = m - 1
-        else:
-            f = m + 1
-    return False
+        ind = (math.log(n,2))
+        for i in range(2,int (ind)):
+            first, last = 2, n
+            while(first <= last):
+                mid = first + int((last - first)/2)
+                print(first,"\t",last,"\t",i,"\t",mid,"\t",n)
+                if n == mid ** i:
+                    return True
+                elif n > mid ** i:
+                    first = mid + 1
+                elif first == last:
+                    if n == first ** i:
+                        return True
+                else:
+                    last = mid - 1
+        return False
