c6d30841268a56c58ad5138bf44ac34c1bd6c2e2
A Jain <foreverrecompin@gmail.com>
2015-08-13 19:02:20 +0530

    added merge sort counting inversions problem

---
+++ python/count_inversions.py
@@ -0,0 +1,33 @@
+def merge_sort(lst, inversions = 0):
+    if len(lst)>1:
+        mid = int(len(lst)/2)
+        left = lst[:mid]
+        right = lst[mid:]
+        left,inv_l = merge_sort(left)
+        right,inv_r = merge_sort(right)
+        merged,inv_lr = merge(left,right)
+        return merged,(inv_l+inv_r+inv_lr)
+    else:
+        return lst,0
+
+def merge(left,right):
+    aux_lst = []
+    i, j = 0, 0
+    inversion_count = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            aux_lst.append(left[i])
+            i += 1
+        else:
+            aux_lst.append(right[j])
+            inversion_count += len(left) - (i)
+            j +=1 
+
+    while i < len(left):
+            aux_lst.append(left[i])
+            i  += 1
+    while j < len(right):
+            aux_lst.append(right[j])
+            j +=1
+
+    return aux_lst,inversion_count
